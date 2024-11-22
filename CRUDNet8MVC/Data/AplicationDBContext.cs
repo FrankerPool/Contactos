931e4dd320fb5c336a3452be0f921d1b83409d0b
FrankerPool <39344039+FrankerPool@users.noreply.github.com>
2024-11-22 12:55:03 -0600

    Add: AplicationContext

---
+++ CRUDNet8MVC/Data/AplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDNet8MVC.Data
+{
+    public class AplicationDBContext : DbContext
+    {
+        public AplicationDBContext(DbContextOptions <AplicationDBContext> options) : base(options)
+        {
+            
+        }
+    }
+}
