85e4afd84078c1b8a3580adf2aaf3986d67023bb
FrankerPool <39344039+FrankerPool@users.noreply.github.com>
2024-11-25 16:55:29 -0600

    Modify: view html index

--- CRUDNet8MVC/Controllers/HomeController.cs
+++ CRUDNet8MVC/Controllers/HomeController.cs
@@ -1,21 +1,24 @@
+using CRUDNet8MVC.Data;
 using CRUDNet8MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CRUDNet8MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        //private readonly ILogger<HomeController> _logger;
+        private readonly AplicationDBContext _context;
+        public HomeController(AplicationDBContext context)
         {
-            _logger = logger;
+            _context = context;
+            ///_logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.ModelContact.ToListAsync());
         }
 
         public IActionResult Privacy()
