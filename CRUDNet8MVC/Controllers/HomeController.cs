d8f328a0166c6a39efc22c3934624b9800cc1120
FrankerPool <39344039+FrankerPool@users.noreply.github.com>
2024-11-28 15:50:51 -0600

    Add: Delete edit and view detail

--- CRUDNet8MVC/Controllers/HomeController.cs
+++ CRUDNet8MVC/Controllers/HomeController.cs
@@ -25,6 +25,48 @@ namespace CRUDNet8MVC.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+
+            }
+            var contact = _context.ModelContact.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return View(contact);
+        }
+        [HttpGet]
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+
+            }
+            var contact = _context.ModelContact.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return View(contact);
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+
+            }
+            var contact = _context.ModelContact.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return View(contact);
+        }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Contact contact)
@@ -37,6 +79,31 @@ namespace CRUDNet8MVC.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteContact(int? id)
+        {
+            var contact = await _context.ModelContact.FindAsync(id);
+            if(contact == null)
+            {
+                return View("Error");
+            }
+            _context.ModelContact.Remove(contact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
         public IActionResult Privacy()
         {
